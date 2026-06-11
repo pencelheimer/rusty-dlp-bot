5f9c8095965e15a48898cf174ae7d03d6abd7814
pencelheimer <pencelheimer@proton.me>
2026-06-11 13:38:03 +0300

    feat: load .env

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
+use tracing::warn;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // TODO(pencelheimer): enable tracing for the CLI or journald
+
+    let _ = dotenvy::dotenv().inspect_err(|_| warn!("`.env` is not loaded"));
+
     Ok(())
 }
